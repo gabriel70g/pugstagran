5be837837e129e3507d6280176eb1fcea59aafd8
Gabriel Gomez <gabriel70@gmail.com>
2020-09-01 21:16:15 -0300

    Maqueta Funcional finalizada

---
+++ src/containers/App.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { onMount } from 'svelte';
+    import Card from '../components/Card.svelte';
+    import Comments from '../components/Comments.svelte';
+    import Footer from '../components/Footer.svelte';
+    import Header from '../components/Header.svelte';
+    import Main from '../components/Main.svelte';
+    import Sidebar from '../components/Sidebar.svelte';
+    import TimeLine from '../components/TimeLine.svelte';
+
+    /*llamada a la api*/ 
+    const API = 'https://pugstagram-ga.firebaseio.com/.json'
+    let data = {}
+    onMount(async () =>{
+        const response = await fetch(API);
+        data = await response.json();
+        console.log(data);
+    })
+
+
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+    :global(body){
+        background-color: #fafafa;
+        color:rgba(38,38,38,07);
+        font-family: 'Lato', sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
+    :global(h1,h2,h3){
+        margin: 0;
+        padding: 0;
+    }
+     
+</style>
+
+<Header />
+<Main>
+    <TimeLine posts={data.posts} />
+    <Sidebar {...data.user}/>
+</Main>
